8a200da755c3deb702ff04c8e3e5b9c21fbd0d1b
acedward@ksu.edu <acedward@ksu.edu>
2018-10-22 15:23:18 -0500

    Added text tab and text panel

---
+++ src/components/text-editor/text-editor.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import {injectIntl} from 'react-intl';
+
+const TextEditor = () => (
+    <div>
+        {'Not Implemented Yet'}
+    </div>
+);
+
+export default injectIntl(TextEditor);
