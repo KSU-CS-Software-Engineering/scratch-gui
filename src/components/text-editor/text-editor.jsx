ba963156d35ad39d7077c04cd1b4c5036ec01ba1
Conor Edwards <lancerspride@outlook.com>
2018-11-09 15:04:59 -0600

    Added ability to check if there are blocks in VM

--- src/components/text-editor/text-editor.jsx
+++ src/components/text-editor/text-editor.jsx
@@ -1,9 +1,32 @@
 import React from 'react';
 import {injectIntl} from 'react-intl';
 
-const TextEditor = () => (
+const printCurrentBlocks = vm => {
+    if (vm.runtime.targets[1]) {
+        if (Object.keys(vm.runtime.targets[1].blocks._blocks).length > 0) {
+            const blocks = vm.runtime.targets[1].blocks._blocks;
+
+            Object.entries(blocks).forEach(
+                ([key, object]) => console.log(key, object)
+            );
+            
+            return (
+                <div>
+                    {'Blocks'}
+                </div>
+            );
+        }
+    }
+    return (
+        <div>
+            {'No Blocks'}
+        </div>
+    );
+};
+
+const TextEditor = props => (
     <div>
-        {'Not Implemented Yet'}
+        {printCurrentBlocks(props.vm)}
     </div>
 );
 
--- src/containers/text-editor.jsx
+++ src/containers/text-editor.jsx
@@ -1,8 +1,21 @@
 import React from 'react';
 import TextEditorComponent from '../components/text-editor/text-editor.jsx';
 
-const TextEditor = () => (
-    <TextEditorComponent />
-);
+class TextEditor extends React.Component {
+    constructor (props) {
+        super(props);
+        this.state = {
+            blocks: null
+        };
+    }
+
+    render () {
+        const {vm} = this.props;
+
+        return (
+            <TextEditorComponent vm={vm} />
+        );
+    }
+}
 
 export default TextEditor;
